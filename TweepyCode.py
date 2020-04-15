8b42cf196a299b8c8d52dd8ae06297beba3b1a34
DavidLittleRock <49722047+DavidLittleRock@users.noreply.github.com>
2020-04-15 10:05:22 -0500

    Update TweepyCode.py
    remove api key

--- TweepyCode.py
+++ TweepyCode.py
@@ -10,10 +10,10 @@ def get_api(cfg):
 def main():
   # Fill in the values noted in previous step here
   cfg = { 
-    "consumer_key"        : "fuuAgy3pElOz4Ecy4ZF1OSuRh",
-    "consumer_secret"     : "7XZnEh5PwXHQ2AH8Ma52g41aAB0D2cTvDSbPaHnzErPSzDAnZt",
-    "access_token"        : "1247287333478699008-KKpYUUHBBxDpJwudh525izgb1EZOVF",
-    "access_token_secret" : "ZDNOfmy2iV9dGNEce14Bgqym2g4j2cEhpibGaztv9GxVc" 
+    "consumer_key"        : "x",
+    "consumer_secret"     : "x",
+    "access_token"        : "x",
+    "access_token_secret" : "x" 
     }
 
   api = get_api(cfg)
